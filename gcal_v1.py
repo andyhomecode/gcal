0597ca8d83132195a3f6d2c8399a2c9b83655d9a
Michele Maxwell <michele@Micheles-MacBook-Air.local>
2016-08-06 19:16:38 -0400

    works great

--- gcal_v1.py
+++ gcal_v1.py
@@ -2,11 +2,15 @@
 from __future__ import print_function
 import httplib2
 import os
+import sys, getopt
 
 from apiclient import discovery
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
+
+# my weather forecast machine
+import andyweather
 
 import datetime
 from pyfiglet import Figlet
@@ -16,23 +20,12 @@ from pyfiglet import Figlet
 import dateutil.parser
 
 
-# todo incorporate the weather objects
-# get the weather from noaa
-# parse it into an object that looks like this:
-#
-# forecast
-#    days[0..6]
-#        [datetime, textForecast]
 
-
-# todo Add option to pick the week, or print out an entire month at a time
-# options for weekly layout?
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+#try:
+#    import argparse
+#    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+#except ImportError:
+#    flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
@@ -74,7 +67,7 @@ def get_credentials():
 def gt(dt_str):
     return dateutil.parser.parse(dt_str)
 
-def main():
+def main(weeksAhead=0):
     """Shows basic usage of the Google Calendar API.
 
     Creates a Google Calendar API service object and outputs a list of the next
@@ -91,9 +84,13 @@ def main():
         sunday = datetime.datetime.today()
     else:
         sunday = datetime.datetime.today()-datetime.timedelta(days=datetime.datetime.today().weekday()+1)
+
+    # add or subtract weeks based on the passed parameter
+    sunday = sunday + datetime.timedelta(weeks=weeksAhead)
     
     sundayiso = sunday.isoformat() + 'Z' # 'Z' indicates UTC time
-    # print('Getting the upcoming 10 events')
+   
+
     eventsResult = service.events().list(
         calendarId='primary', timeMin=sundayiso, maxResults=100, singleEvents=True,
         orderBy='startTime').execute()
@@ -102,32 +99,55 @@ def main():
     #if not events:
     #    print('No upcoming events found.')
 
+    # here's the NOAA URL for NYC, and get the weather in a nice array of date and forecast
+    weatherurl = 'http://forecast.weather.gov/MapClick.php?lat=40.7273&lon=-73.9807&FcstType=dwml'
+
+    # now go get the weather forecast.  Returns a list of lists
+    # [[datetime], [forecast]]
+    weatherArray = andyweather.getWeather(weatherurl)
+
     # days of week, in calendar display order, not Python order
     dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-
+    # I loooove me some big fonts
     f = Figlet(font='big')
     print(f.renderText("Maxwell Calendar"))
 
     # This loops through the week, starting on Sunday thru Saturday
     for i in range(7):
         currentday = sunday + datetime.timedelta(days=i)
-        print(dow[i], currentday.date())
+        print("---------------------------", dow[i], currentday.date())
 
-        # TODO
-        # print weather forecast (need to finish and import Weather array sucker)
-        # print day footer
-        
+        # loop through all the calendar events and find ones that match the day to print
         for event in events:
             start = gt(event['start'].get('dateTime', event['start'].get('date')))
             end = gt(event['end'].get('dateTime', event['end'].get('date')))
             if start.date() == currentday.date():
                 if start == end - datetime.timedelta(days=1): # All-day events have no time, and the end time is midnight the next day
-                    print("  ", event['summary'])
+                    print(event['summary'])
                 else:
-                    print("  ", event['summary'], start.time())
+                    print(event['summary'], start.time())
     
+        # loop through the weather array putting out the forecast
+        for j in weatherArray:
+            if j[0].date() == currentday.date():
+                # NOAA sets the time at 6am for the morning forecast and 6pm for the evening
+                if j[0].hour == 6:
+                    print("   ... Morning:", j[1])
+                elif j[0].hour == 18:
+                    print("   ... Evening:", j[1])
+                else:
+                    print("   ... Forecast:", j[1])
+
             
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "next":
+            main(1)
+        elif sys.argv[1] == "previous":
+            main(-1)
+        else:
+            main()
+    else:
+        main()
